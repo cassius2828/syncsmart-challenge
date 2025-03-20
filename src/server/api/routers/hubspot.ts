0092b299cd394ff3d42ea76cd6856090203a6aac
Cassius <cassius.reynolds.dev@gmail.com>
2025-03-20 08:13:29 -0700

    fixed issue where could not get phone number to come with data in get req. Added queries manually to fix

--- src/server/api/routers/hubspot.ts
+++ src/server/api/routers/hubspot.ts
@@ -6,8 +6,11 @@ import { type Contact } from "coding-challenge/utils/types";
 // batching causes an issue when some of the data has already been transferred
 // may be better to either do iterate and do every one, or have a fall back of iteration, or
 // lastly try removing ids that fail and try again
+
+// * Batch = 1 call, all data in or none in
+// * Single = allows succeeding data to prevail and failed to stay out, also allows for optional fields to be included
 const batchContactsCreateURL = `https://api.hubapi.com/crm/v3/objects/contacts/batch/create`;
-const readObjectContactsURL = `https://api.hubapi.com/crm/v3/objects/contacts`;
+const readObjectContactsURL = `https://api.hubapi.com/crm/v3/objects/contacts?properties=phone&properties=firstname&properties=lastname&properties=email`;
 
 const contactInputSchema = z.array(
   z.object({
